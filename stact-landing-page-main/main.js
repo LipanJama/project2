46afaed79396f898388fd64c7c06cba50d7f7ebe
AbdiazizAbdulle <cabdicasiiscabdullahi53@gmail.com>
2024-06-04 13:23:00 +0300

    I was added first project

---
+++ stact-landing-page-main/main.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector(".burger");
+
+burger.addEventListener("click", function () {
+    // console.log("click");
+    document.querySelector(".menu").classList.toggle("active");
+});
